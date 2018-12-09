cd86c93b07c2d7898dd4cd8df1d61dda8b417042
3bker <44803452+3bker@users.noreply.github.com>
2018-12-09 18:19:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client.on("message", function(message) {
     var args = message.content.split(/ +/g);
     var command = args.shift()
     
-    if(command == "m11s") {
+    if(command == "m15s") {
         message.channel.send(args.slice(1, args.length).join(" "))    
     }
 });
